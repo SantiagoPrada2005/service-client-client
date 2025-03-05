c5d6564e5aee9429af383494d6706e9f604920d8
Brian <bsquintero_usistemas@intep.edu.co>
2025-03-05 10:59:50 -0500

    Se modifico el nombre del directorio pages por login y se agrego un nuevo directorio llamado signup

---
+++ service-client-ai/src/app/signup/page.js
@@ -0,0 +1,67 @@
+'use client';
+import { useState } from 'react';
+import styles from './css/signup.module.css';
+
+export default function LoginPage() {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // Aquí irá tu lógica de autenticación
+      console.log('Datos del formulario:', formData);
+    } catch (error) {
+      console.error('Error al iniciar sesión:', error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prev => ({
+      ...prev,
+      [name]: value
+    }));
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.loginBox}>
+          <div className={styles.imageContainer}>
+          </div>
+          <div className={styles.loginElements}>
+          <h1>Iniciar Sesión</h1>
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <div className={styles.formGroup}>
+              <label htmlFor="email">Email</label>
+              <input
+                type="user"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="password">Contraseña</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <button type="submit" className={styles.button}>
+              Iniciar Sesión
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
