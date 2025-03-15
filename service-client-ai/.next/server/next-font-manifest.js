50f14a8963a4ed0533e260c4c1265c0862dffbf8
SantiagoPrada2005 <116417241+SantiagoPrada2005@users.noreply.github.com>
2025-03-14 23:49:39 -0500

    Optimizacion de directorios

--- service-client-ai/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/santiagopradamoreno/Documents/ServiceClient/service-client-client/service-client-ai/src/app/layout":["static/media/569ce4b8f30dc480-s.p.woff2","static/media/93f479601ee12b01-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
